b535e86f1a71b2fb66ab79d28b6c7b282f5d9c81
sashaus <sashaus359@gmail.com>
2023-07-14 15:22:09 +0300

    added fake tests

---
+++ src/test/java/com/sashaus/tests/FakeTests.java
@@ -0,0 +1,12 @@
+package com.sashaus.tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("fake_test")
+public class FakeTests {
+
+    @Test
+    void fakeTest1() {
+    }
+}
